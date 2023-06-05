bb2865907143ebcabf8684e9c0c48b915fd5031d
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-06-05 16:26:40 +0100

    loading comparision pdb

--- molicule-visualizer/src/Components/ComprisionComponent.jsx
+++ molicule-visualizer/src/Components/ComprisionComponent.jsx
@@ -11,20 +11,52 @@ export default class ComparisonComponent extends Component {
     const stage = new NGL.Stage(this.container.current);
 
     const loadProteins = async () => {
-      const protein1 = await stage.loadFile('rcsb://1CRN');
-      const protein2 = await stage.loadFile('rcsb://1CSE');
+      const protein1 = await stage.loadFile('http://localhost:3003/TestProteans/SuperImposed.pdb.pdb');
+      const protein2 = await stage.loadFile('rcsb://1CSE.pdb');
 
       // Add representations for the proteins
       protein1.addRepresentation('cartoon', { color: 'red' });
       protein2.addRepresentation('cartoon', { color: 'blue' });
 
-      // Create a diff representation
-    //   const diff = new NGL.DiffRepresentation(protein1, protein2);
-    //   diff.create();
+      // Assuming you have the rotation matrix values stored in variables
+const rotationMatrix = [
+  [-0.1429300663, -0.9642663321, -0.2230727166],
+  [-0.7725864851, -0.0321728578, 0.6340938655],
+  [-0.6186122527, 0.2629740443, -0.7403806675]
+];
 
-    // Superimpose the proteins
-    //   const align = new NGL.Align(protein1, protein2);
-     //   align.run();
+const translationVector = [0, -23.5534790988, 9.5513851519];
+
+// Iterate over each molecule in the structure
+for (let i = 0; i < protein1.structure.numMolecules; i++) {
+  const molecule = protein1.structure.getMoleculeProxy(i);
+
+  // Iterate over each atom in the molecule
+  for (let j = 0; j < molecule.atomCount; j++) {
+    const atom = molecule.atomProxy[j];
+
+    // Get the atom coordinates
+    const x = atom.x;
+    const y = atom.y;
+    const z = atom.z;
+
+    // Apply the rotation transformation
+    const X = translationVector[0] + rotationMatrix[0][0] * x + rotationMatrix[0][1] * y + rotationMatrix[0][2] * z;
+    const Y = translationVector[1] + rotationMatrix[1][0] * x + rotationMatrix[1][1] * y + rotationMatrix[1][2] * z;
+    const Z = translationVector[2] + rotationMatrix[2][0] * x + rotationMatrix[2][1] * y + rotationMatrix[2][2] * z;
+
+    // Update the atom coordinates
+    atom.x = X;
+    atom.y = Y;
+    atom.z = Z;
+  }
+}
+      //console.log(protein1.structure);
+// const superposition = new NGL.Superposition(protein1.structure, protein2.structure);
+//  protein1.setTransform(superposition.transformationMatrix); 
+  //protein2.setTransform(superposition.transformationMatrix);
+//superposition.align();
+    protein1.updateRepresentations({position: true});
 
       stage.autoView();
       stage.viewer.requestRender();
