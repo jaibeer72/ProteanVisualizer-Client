6110f1b95a4a27ee7443f1b49c2dceaa636f277b
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-07-11 18:11:36 +0100

    basic redux setup to run things on overlays

--- molicule-visualizer/src/Components/ProteinVisualizerComponent.jsx
+++ molicule-visualizer/src/Components/ProteinVisualizerComponent.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 import * as NGL from 'ngl';
+import OverlyComponent from './OverlyComponent';
+import { addOverlay , removeOverlay , changeColor} from '../Stores/Reducers/OverlayReducer';
 
-export default class ProteinVisualizerComponent extends Component {
+
+class ProteinVisualizerComponent extends Component {
   static propTypes = {
     proteinPaths: PropTypes.arrayOf(PropTypes.string).isRequired,
-    shoudlDisplaySuperImposed: PropTypes.bool.isRequired //TODO: maybe see if this is required
+    shoudlDisplaySuperImposed: PropTypes.bool.isRequired,
+    addOverlay: PropTypes.func.isRequired,
+    removeOverlay: PropTypes.func.isRequired,
+    changeColor: PropTypes.func.isRequired,
   }
 
   constructor(props) {
@@ -13,37 +20,56 @@ export default class ProteinVisualizerComponent extends Component {
     this.container = React.createRef();
     this.stage = null; // Store the NGL.Stage instance here
     this.proteanArray = [];
+    this.overlay = this.props.addOverlay({color: "red"});
   }
 
     componentDidMount() {
-    const stage = new NGL.Stage(this.container.current);
+    this.stage = new NGL.Stage(this.container.current);
 
     const loadProteins = async () => {
         this.proteanArray = [];
         for (let i = 0; i < this.props.proteinPaths.length; i++) {
-            this.proteanArray.push(await stage.loadFile(this.props.proteinPaths[i]));
-            this.proteanArray[i].addRepresentation('cartoon');
+            this.proteanArray.push(await this.stage.loadFile(this.props.proteinPaths[i]));
+            this.proteanArray[i].addRepresentation('cartoon',{color: this.overlay.color});
             this.proteanArray[i].autoView();
         }
-        stage.autoView();
-        stage.viewer.requestRender();
+        this.stage.autoView();
+        this.stage.viewer.requestRender();
         // Add representations for the proteins
     }
     loadProteins();
+    }  
+
+    componentDidUpdate(prevProps) {
+        this.proteanArray.forEach((protean) => {
+        protean.addRepresentation('cartoon',{color: this.overlay.color});
+        });
+        this.stage.autoView();
+        this.stage.viewer.requestRender();
     }
   render() {
     return (
     <div className='visualizer-container '>
       <div ref={this.container} className="ngl-container" >
-      <div className="overlay">
-      <h3>UI Overlay Panel</h3>
-      <button>Button 1</button>
-      <button>Button 2</button>
-      <button>Button 3</button>
-      </div>
+          <OverlyComponent changeColor={this.props.changeColor} overlayID={this.props.overlay.id}/>
       </div>
     </div>
     );
   }
 }
 
+// Map Redux state to React component props
+const mapStateToProps = (state) => {
+  return {
+    overlays: state.overlays.overlays,
+  };
+};
+
+
+const mapDispatchToProps = {
+  addOverlay,
+  removeOverlay,
+  changeColor,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProteinVisualizerComponent);
